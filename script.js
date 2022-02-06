a15362b20e412cbafea4dcbb885f4ec3c813875f
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-06 20:37:24 +0500

    Logic part 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 const message = document.querySelector('.message');
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -11,6 +11,12 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     message.textContent = '🎉 Correct Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       message.textContent = '📈 Too high';
@@ -31,4 +37,12 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
-document.querySelector('.again').addEventListener('click', function () {});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  message.textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  const guess = Number((document.querySelector('.guess').value = ''));
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+});
