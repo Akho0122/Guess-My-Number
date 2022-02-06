609d839f0cc8685da1fe0af087603df8b6212c1b
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-06 22:16:37 +0500

    Again Part - 3

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 'use strict';
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 let message = document.querySelector('.message');
 let score = 20;
 let highscore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -13,23 +12,14 @@ document.querySelector('.check').addEventListener('click', function () {
     message.textContent = 'Correct Number 🎉';
     document.querySelector('body').style.backgroundColor = 'yellowgreen';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      message.textContent = '📈 Too high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      score = 0;
-      message.textContent = 'You lose 😭';
-      document.querySelector('.score').textContent = score;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      message.textContent = '📉 Too low';
+      message.textContent = guess > secretNumber ? '📈 Too high' : '📉 Too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
@@ -40,7 +30,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 document.querySelector('.again').addEventListener('click', function () {
-  let score = 20;
+  score = 20;
   document.querySelector('.score').textContent = score;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
