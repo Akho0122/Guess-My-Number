7343a0dbc593344e01f74212555b82ca9a7f2d81
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-06 20:10:32 +0500

    Logic

--- script.js
+++ script.js
@@ -1,54 +1,34 @@
 'use strict';
-
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+const message = document.querySelector('.message');
 let score = 20;
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
-  const displayMessage = function (message) {
-    document.querySelector('.message').textContent = message;
-  };
-
-  // When there is no input
   if (!guess) {
-    displayMessage('⛔ No number');
-
-    // When player wins
+    message.textContent = '⛔ No number';
   } else if (guess === secretNumber) {
-    displayMessage('🎉 Correct Number!');
+    message.textContent = '🎉 Correct Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
-
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
-    }
-
-    // When guess is wrong
-  } else if (guess !== secretNumber) {
+  } else if (guess > secretNumber) {
     if (score > 1) {
-      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      message.textContent = '📈 Too high';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      displayMessage('💥 You lost the game!');
+      message.textContent = 'You lose the game 😭';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      message.textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      message.textContent = 'You lose the game 😭';
       document.querySelector('.score').textContent = 0;
     }
   }
 });
-
-const again = document
-  .querySelector('.again')
-  .addEventListener('click', function () {
-    score = 20;
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.score').textContent = score;
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.guess').value = '';
-    displayMessage('Start guessing ...');
-  });
+document.querySelector('.again').addEventListener('click', function () {});
